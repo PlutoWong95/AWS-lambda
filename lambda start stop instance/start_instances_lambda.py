ebb517dc8d9a3fd94056faed4606bda70782e596
PlutoWong <98079588+PlutoWong95@users.noreply.github.com>
2023-10-19 11:51:57 +0800

    create instances start stop functions
    policy and python function

---
+++ lambda start stop instance/start_instances_lambda.py
@@ -0,0 +1,14 @@
+#waiting to test
+
+import json
+import boto3
+
+ec2 = boto3.resource('ec2', region_name='ap-east-1')
+def lambda_handler(event, context):
+   #type your specify tag name here, suggest use bool to identify the value (True/False)
+   instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']},{'Name': 'tag:tag_name_here','Values':['True']}])
+   for instance in instances:
+       id=instance.id
+       ec2.instances.filter(InstanceIds=[id]).start()
+       print("Instance ID is stopped:- "+instance.id)
+   return "success" 
